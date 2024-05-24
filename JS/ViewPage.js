e917c651c6f47cbccc7a9ff5ad2668c161879ec4
Erick Paes <Ems2201@outlook.com>
2024-05-24 09:19:38 -0300

    Corrigindo CSS e Refatorando Código da ViewPage

--- JS/ViewPage.js
+++ JS/ViewPage.js
@@ -5,22 +5,22 @@ document.addEventListener('DOMContentLoaded', function() {
         const selectValue = select.value; // Moveu esta linha para dentro do evento de mudança
         switch (selectValue) {
             case "firstFit":
-                firstFit();
+                firstFit('normal');
                 break;
             case "firstFit2":
-                firstFit2();
+                firstFit2('Crescente');
                 break;
             case "firstFit3":
-                firstFit3();
+                firstFit3('Decrescente');
                 break;
             case "bestFit":
-                bestFit();
+                bestFit('normal');
                 break;
             case "bestFit2":
-                bestFit2();
+                bestFit2('Crescente');
                 break;
             case "bestFit3":
-                bestFit3();
+                bestFit3('Decrescente');
                 break;
             default:
                 break;
@@ -37,22 +37,27 @@ document.addEventListener('DOMContentLoaded', function() {
     
 });
 
-function firstFit() {
-    const data = JSON.parse(sessionStorage.getItem('data'));
+function firstFit(sortType = 'normal') {
+    let data = JSON.parse(sessionStorage.getItem('data'));
 
+    switch (sortType) {
+        case 'Crescente':
+            data.sort((a, b) => a.altura - b.altura);
+            break;
+        case 'Decrescente':
+            data.sort((a, b) => b.altura - a.altura);
+            break;
+        // 'none' case doesn't need sorting
+    }
 
     const columnCapacity = 1800;
-    let scale = columnCapacity/350;
-    
-
+    let scale = columnCapacity / 400;
 
     let columns = [];
 
-    // Remove todas as bins e itens anteriores
     const container = document.getElementById('bin');
     container.innerHTML = '';
 
-    // Realiza o algoritmo First Fit com os dados do objeto data
     for (let k = 0; k < data.length; k++) {
         const uf = data[k];
         const { nome, altura, valor, quantidade } = uf;
@@ -66,7 +71,7 @@ function firstFit() {
                     column.ufs.push({ nome, altura, valor });
                     column.currentCapacity += altura;
                     columnFound = true;
-                    break; // item alocado, sai do loop
+                    break;
                 }
             }
 
@@ -80,8 +85,7 @@ function firstFit() {
         }
     }
 
-    // Exibe os bins na página
-    columns.forEach((column, index) => {
+    columns.forEach((column) => {
         const columnElement = document.createElement('div');
         columnElement.classList.add('bins');
         container.appendChild(columnElement);
@@ -92,154 +96,31 @@ function firstFit() {
             ufElement.textContent = `${uf.nome} ${uf.valor}`;
             ufElement.style.height = (uf.altura / scale) + 'px';
             columnElement.appendChild(ufElement);
-
-        
         });
     });
-    
-   
-    
-    
 }
 
-function firstFit2() {
-    const data = JSON.parse(sessionStorage.getItem('data'));
+function bestFit(sortType = 'normal') {
+    let data = JSON.parse(sessionStorage.getItem('data'));
 
-    // Ordena os dados do objeto data em ordem crescente de altura
-    data.sort((a, b) => a.altura - b.altura);
-
-    const columnCapacity = 1800;
-    let scale = columnCapacity / 350;
-
-    let columns = [];
-
-    // Remove todas as bins e itens anteriores
-    const container = document.getElementById('bin');
-    container.innerHTML = '';
-
-    // Realiza o algoritmo First Fit com os dados do objeto data
-    for (let k = 0; k < data.length; k++) {
-        const uf = data[k];
-        const { nome, altura, valor, quantidade } = uf;
-
-        for (let j = 0; j < quantidade; j++) {
-            let columnFound = false;
-
-            for (let i = 0; i < columns.length; i++) {
-                let column = columns[i];
-                if (column.currentCapacity + altura <= columnCapacity) {
-                    column.ufs.push({ nome, altura, valor });
-                    column.currentCapacity += altura;
-                    columnFound = true;
-                    break; // item alocado, sai do loop
-                }
-            }
-
-            if (!columnFound) {
-                const newColumn = {
-                    currentCapacity: altura,
-                    ufs: [{ nome, altura, valor }]
-                };
-                columns.push(newColumn);
-            }
-        }
+    switch (sortType) {
+        case 'Crescente':
+            data.sort((a, b) => a.altura - b.altura);
+            break;
+        case 'Decrescente':
+            data.sort((a, b) => b.altura - a.altura);
+            break;
+        // 'none' case doesn't need sorting
     }
 
-    // Exibe os bins na página
-    columns.forEach((column, index) => {
-        const columnElement = document.createElement('div');
-        columnElement.classList.add('bins');
-        container.appendChild(columnElement);
-
-        column.ufs.forEach(uf => {
-            const ufElement = document.createElement('div');
-            ufElement.classList.add('item');
-            ufElement.textContent = `${uf.nome} ${uf.valor}`;
-            ufElement.style.height = (uf.altura / scale) + 'px';
-            columnElement.appendChild(ufElement);
-        });
-    });
-
-    // Limpa os dados do sessionStorage
-}
-
-function firstFit3() {
-    const data = JSON.parse(sessionStorage.getItem('data'));
-
-    // Ordena os dados do objeto data em ordem crescente de altura
-    data.sort((a, b) => b.altura - a.altura);
-
     const columnCapacity = 1800;
-    let scale = columnCapacity / 350;
+    let scale = columnCapacity / 400;
 
     let columns = [];
 
-    // Remove todas as bins e itens anteriores
     const container = document.getElementById('bin');
     container.innerHTML = '';
 
-    // Realiza o algoritmo First Fit com os dados do objeto data
-    for (let k = 0; k < data.length; k++) {
-        const uf = data[k];
-        const { nome, altura, valor, quantidade } = uf;
-
-        for (let j = 0; j < quantidade; j++) {
-            let columnFound = false;
-
-            for (let i = 0; i < columns.length; i++) {
-                let column = columns[i];
-                if (column.currentCapacity + altura <= columnCapacity) {
-                    column.ufs.push({ nome, altura, valor });
-                    column.currentCapacity += altura;
-                    columnFound = true;
-                    break; // item alocado, sai do loop
-                }
-            }
-
-            if (!columnFound) {
-                const newColumn = {
-                    currentCapacity: altura,
-                    ufs: [{ nome, altura, valor }]
-                };
-                columns.push(newColumn);
-            }
-        }
-    }
-
-    // Exibe os bins na página
-    columns.forEach((column, index) => {
-        const columnElement = document.createElement('div');
-        columnElement.classList.add('bins');
-        container.appendChild(columnElement);
-
-        column.ufs.forEach(uf => {
-            const ufElement = document.createElement('div');
-            ufElement.classList.add('item');
-            ufElement.textContent = `${uf.nome} ${uf.valor}`;
-            ufElement.style.height = (uf.altura / scale) + 'px';
-            columnElement.appendChild(ufElement);
-        });
-    });
-
-    // Limpa os dados do sessionStorage
-}
-
-
-function bestFit() {
-    const data = JSON.parse(sessionStorage.getItem('data'));
-
-    // Ordena os dados do objeto data em ordem crescente de altura
-
-    const columnCapacity = 1800;
-    let scale = columnCapacity / 350;
-
-    let columns = [];
-
-    // Remove todas as bins e itens anteriores
-    const container = document.getElementById('bin');
-    container.innerHTML = '';
-
-    // Realiza o algoritmo Best Fit com os dados do objeto data
     for (let k = 0; k < data.length; k++) {
         const uf = data[k];
         const { nome, altura, valor, quantidade } = uf;
@@ -272,8 +153,7 @@ function bestFit() {
         }
     }
 
-    // Exibe os bins na página
-    columns.forEach((column, index) => {
+    columns.forEach((column) => {
         const columnElement = document.createElement('div');
         columnElement.classList.add('bins');
         container.appendChild(columnElement);
@@ -286,142 +166,4 @@ function bestFit() {
             columnElement.appendChild(ufElement);
         });
     });
-
-    // Limpa os dados do sessionStorage
-    
-}
-
-function bestFit2() {
-    const data = JSON.parse(sessionStorage.getItem('data'));
-
-    // Ordena os dados do objeto data em ordem crescente de altura
-    data.sort((a, b) => a.altura - b.altura);
-
-    const columnCapacity = 1800;
-    let scale = columnCapacity / 350;
-
-    let columns = [];
-
-    // Remove todas as bins e itens anteriores
-    const container = document.getElementById('bin');
-    container.innerHTML = '';
-
-    // Realiza o algoritmo Best Fit com os dados do objeto data
-    for (let k = 0; k < data.length; k++) {
-        const uf = data[k];
-        const { nome, altura, valor, quantidade } = uf;
-
-        for (let j = 0; j < quantidade; j++) {
-            let bestColumnIndex = -1;
-            let minWaste = Number.MAX_VALUE;
-
-            for (let i = 0; i < columns.length; i++) {
-                let column = columns[i];
-                if (column.currentCapacity + altura <= columnCapacity) {
-                    let waste = columnCapacity - (column.currentCapacity + altura);
-                    if (waste < minWaste) {
-                        minWaste = waste;
-                        bestColumnIndex = i;
-                    }
-                }
-            }
-
-            if (bestColumnIndex !== -1) {
-                columns[bestColumnIndex].ufs.push({ nome, altura, valor });
-                columns[bestColumnIndex].currentCapacity += altura;
-            } else {
-                const newColumn = {
-                    currentCapacity: altura,
-                    ufs: [{ nome, altura, valor }]
-                };
-                columns.push(newColumn);
-            }
-        }
-    }
-
-    // Exibe os bins na página
-    columns.forEach((column, index) => {
-        const columnElement = document.createElement('div');
-        columnElement.classList.add('bins');
-        container.appendChild(columnElement);
-
-        column.ufs.forEach(uf => {
-            const ufElement = document.createElement('div');
-            ufElement.classList.add('item');
-            ufElement.textContent = `${uf.nome} ${uf.valor}`;
-            ufElement.style.height = (uf.altura / scale) + 'px';
-            columnElement.appendChild(ufElement);
-        });
-    });
-
-    // Limpa os dados do sessionStorage
-    
-}
-
-
-function bestFit3() {
-    const data = JSON.parse(sessionStorage.getItem('data'));
-
-    // Ordena os dados do objeto data em ordem crescente de altura
-    data.sort((a, b) => b.altura - a.altura);
-
-    const columnCapacity = 1800;
-    let scale = columnCapacity / 350;
-
-    let columns = [];
-
-    // Remove todas as bins e itens anteriores
-    const container = document.getElementById('bin');
-    container.innerHTML = '';
-
-    // Realiza o algoritmo Best Fit com os dados do objeto data
-    for (let k = 0; k < data.length; k++) {
-        const uf = data[k];
-        const { nome, altura, valor, quantidade } = uf;
-
-        for (let j = 0; j < quantidade; j++) {
-            let bestColumnIndex = -1;
-            let minWaste = Number.MAX_VALUE;
-
-            for (let i = 0; i < columns.length; i++) {
-                let column = columns[i];
-                if (column.currentCapacity + altura <= columnCapacity) {
-                    let waste = columnCapacity - (column.currentCapacity + altura);
-                    if (waste < minWaste) {
-                        minWaste = waste;
-                        bestColumnIndex = i;
-                    }
-                }
-            }
-
-            if (bestColumnIndex !== -1) {
-                columns[bestColumnIndex].ufs.push({ nome, altura, valor });
-                columns[bestColumnIndex].currentCapacity += altura;
-            } else {
-                const newColumn = {
-                    currentCapacity: altura,
-                    ufs: [{ nome, altura, valor }]
-                };
-                columns.push(newColumn);
-            }
-        }
-    }
-
-    // Exibe os bins na página
-    columns.forEach((column, index) => {
-        const columnElement = document.createElement('div');
-        columnElement.classList.add('bins');
-        container.appendChild(columnElement);
-
-        column.ufs.forEach(uf => {
-            const ufElement = document.createElement('div');
-            ufElement.classList.add('item');
-            ufElement.textContent = `${uf.nome} ${uf.valor}`;
-            ufElement.style.height = (uf.altura / scale) + 'px';
-            columnElement.appendChild(ufElement);
-        });
-    });
-
-    // Limpa os dados do sessionStorage
-    
 }

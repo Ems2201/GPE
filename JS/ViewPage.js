13de63b1cf7bdf868b90011f99d2f3dca52f3c4f
Erick Paes <Ems2201@outlook.com>
2024-06-11 14:35:16 -0300

    Adicionando BF Bin Completion

--- JS/ViewPage.js
+++ JS/ViewPage.js
@@ -18,6 +18,9 @@ document.addEventListener('DOMContentLoaded', function() {
             case "bestFit4":
                 bestFitMelhorado();
                 break;
+            case "bestFit5":
+                bestFitBinCompletion();
+                break;
             default:
                 break;
         }
@@ -126,6 +129,68 @@ function bestFitMelhorado() {
         });
     });
 }
+
+function bestFitBinCompletion() {
+    let data = JSON.parse(sessionStorage.getItem('data'));
+    
+    const columnCapacity = 1800;
+    let scale = columnCapacity / 400;
+
+    let columns = [];
+
+    const container = document.getElementById('bin');
+    container.innerHTML = '';
+
+    for (let k = 0; k < data.length; k++) {
+        const uf = data[k];
+        const { nome, altura, valor, quantidade } = uf;
+
+        for (let j = 0; j < quantidade; j++) {
+            let bestColumnIndex = -1;
+            let minWaste = Number.MAX_VALUE;
+
+            for (let i = 0; i < columns.length; i++) {
+                let column = columns[i];
+                if (column.currentCapacity + altura <= columnCapacity) {
+                    let waste = columnCapacity - (column.currentCapacity + altura);
+                    if (waste < minWaste) {
+                        minWaste = waste;
+                        bestColumnIndex = i;
+                    }
+                }
+            }
+
+            if (bestColumnIndex !== -1) {
+                columns[bestColumnIndex].ufs.push({ nome, altura, valor });
+                columns[bestColumnIndex].currentCapacity += altura;
+            } else {
+                const newColumn = {
+                    currentCapacity: altura,
+                    ufs: [{ nome, altura, valor }]
+                };
+                columns.push(newColumn);
+            }
+        }
+    }
+
+    columns.sort((a, b) => b.currentCapacity - a.currentCapacity);
+
+    columns.forEach((column) => {
+        const columnElement = document.createElement('div');
+        columnElement.classList.add('bins');
+        container.appendChild(columnElement);
+
+        column.ufs.forEach(uf => {
+            const ufElement = document.createElement('div');
+            ufElement.classList.add('item');
+            ufElement.textContent = `${uf.nome} ${uf.valor}`;
+            ufElement.style.height = (uf.altura / scale) + 'px';
+            columnElement.appendChild(ufElement);
+        });
+    });
+}
+
+
 
 function bestFit(sortType = 'normal') {
     let data = JSON.parse(sessionStorage.getItem('data'));
